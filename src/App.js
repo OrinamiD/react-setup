eb30a15220bd9c9070bce9bb6bbba04368d15d76
OrinamiD <dongoorinami@gmail.com>
2026-01-07 01:29:57 +0100

    new line of code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+import Welcome from "./components/Welcome";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to CareerEx</h1>
+    </div>
+  );
+};
+
+export default Welcome;
